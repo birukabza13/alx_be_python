c7ce9004f7e3d9c7b1c0ac8c42b0012fdecbeb69
birukabza <birukabza@gmail.com>
2024-09-09 14:44:04 +0300

    Created basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,6 @@
+number1: int = 10
+number2: int = 5
+
+print(f"Addition of {number1} and {number2} is {number1+number2}")
+print(f"Subtraction of {number1} and {number2} is {number1-number2}")
+print(f"Multiplication of {number1} and {number2} is {number1*number2}")
