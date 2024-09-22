c353a733bc6e68b6873a8c16ff0fb330c1daa460
biruk <birukabza13@gmail.com>
2024-09-22 15:07:57 +0300

    updated the fns_and_data/arithmetic_operations.py file

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -7,8 +7,9 @@ def perform_operation(num1, num2, operation):
         case "multiply":
             return num1 * num2
         case "divide":
-            if num2 == 0:
+            if num2 != 0:
+                return num1 / num2
+            elif num2 == 0:
                 return "Can't divide by zero"
-            return num1 / num2
         case _:
             return "Input the appropriate operation"
