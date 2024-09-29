621ab0f4b3f5b4206329c8c0b176d8896e0d9629
biruk <birukabza13@gmail.com>
2024-09-29 18:00:43 +0300

    creates the file bank_account.py

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,18 @@
+class BankAccount:
+
+
+    def __init__(self, account_balance = 0) -> None:
+        self.account_balance = account_balance
+    
+
+    def depoist(self, amount):
+        self.account_balance+=amount
+    
+    def withdraw(self, amount):
+        if amount > self.account_balance:
+            return False
+        self.account_balance-=amount
+        return False
+
+    def display(self):
+        print(f"Current Balance: {self.account_balance}")
